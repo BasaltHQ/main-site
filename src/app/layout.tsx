ba19822e950d9b4fbf61d406201b67c61af48295
GenRevo89 <krishnapatel@mac.com>
2026-04-21 01:56:44 -0600

    v1.0.1j - Layout ts error patched.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -176,13 +176,13 @@ export default function RootLayout({
           </DemoVideoProvider>
         </ThemeProvider>
         <Script id="clarity-script" strategy="afterInteractive">
-          {\`
+          {`
             (function(c,l,a,r,i,t,y){
                 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
             })(window, document, "clarity", "script", "w23g3kb9zu");
-          \`}
+          `}
         </Script>
       </body>
     </html>
